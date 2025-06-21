fd8fa241dced45e29636a7c6a4afb761a02a7f2b
tikuzaa <dey.debangan2004@gmail.com>
2025-06-21 17:26:16 +0530

    fix

--- app/api/quizzes/questions/[id]/route.ts
+++ app/api/quizzes/questions/[id]/route.ts
@@ -5,16 +5,8 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
 
   const { id } = await params;
-import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
-
-export async function GET(request: Request, { params }: { params: { id: string } }) {
-
-  const { id } = params;
-
-  const questions = await prisma.QuizQuestion.findMany({
+  const questions = await prisma.quizQuestion.findMany({
     where: { quizId:id },
   });
 
